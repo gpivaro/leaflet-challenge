8049067fd72a771ecdaa9030d777cc41e5a7de03
Gabriel Pivaro <gabrconc@gmail.com>
2020-11-29 16:53:57 -0600

    table data and json api

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -6,10 +6,32 @@ d3.json(url).then((data) => {
 
     // Store the imported data to a variable
     var EarthquakesData = data;
-
     // Print the data
     console.log(EarthquakesData);
     // Print the object keys
     console.log(Object.keys(EarthquakesData));
+
+    // Get the date that the data was generated
+    var dataDate = new Date(EarthquakesData.metadata.generated);
+    console.log(`Data retrieved at: ${dataDate}`);
+
+    // Number of data points on the data set
+    console.log(`Number of records: ${EarthquakesData.metadata.count}`);
+
+    // Earthquakes magnitude
+    console.log(EarthquakesData.features[0].properties.mag);
+
+    // Earthquakes time
+    console.log(new Date(EarthquakesData.features[0].properties.time));
+
+    // Earthquakes lat
+    console.log(EarthquakesData.features[0].geometry.coordinates[0]);
+
+    // Earthquakes lon
+    console.log(EarthquakesData.features[0].geometry.coordinates[1]);
+
+    // Earthquakes depth
+    console.log(EarthquakesData.features[0].geometry.coordinates[2]);
+
 
 });
