5e0094c38661c2beb0cf96aafab9b0efc018b470
Gabriel Pivaro <gabrconc@gmail.com>
2020-12-03 18:31:52 -0600

    Zoom level and initial location

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -63,8 +63,8 @@ function createMap(earthquakes, data) {
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("map", {
-        center: [38.09, -98.991],
-        zoom: 4.5,
+        center: [0, 0],
+        zoom: 2,
         layers: [OpenStreetTiles]
 
     });
