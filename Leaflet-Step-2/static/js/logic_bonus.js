2bfee4782db9628d06b0708be116d98330cdbe30
Gabriel Pivaro <gabrconc@gmail.com>
2020-12-18 02:11:22 -0600

    Zoom level

--- Leaflet-Step-2/static/js/logic_bonus.js
+++ Leaflet-Step-2/static/js/logic_bonus.js
@@ -104,8 +104,8 @@ function createMap(earthquakes, tectonicPlates) {
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     var myMap = L.map("map", {
-        center: [0, 0],
-        zoom: 2.5,
+        center: [10, 0],
+        zoom: 2,
         fullscreenControl: true,
         layers: [Stadia_AlidadeSmoothDark, earthquakes, tectonicPlates]
     });
